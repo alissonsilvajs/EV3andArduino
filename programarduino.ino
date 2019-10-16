4ccbfd0635809e97e0a125f946c104862799b5a6
Alisson Silva <40868438+alissonsilvam@users.noreply.github.com>
2019-10-16 02:29:01 +0000

    Update programarduino.ino

--- programarduino.ino
+++ programarduino.ino
@@ -1,26 +1,30 @@
-#include <Wire.h> 
-#define SLAVE_ADDRESS 0x04 
+#include <Wire.h> // Import the I2C library
+#define SLAVE_ADDRESS 0x04 // Define port used to communicate between Arduino and EV3 
 
 int s2;
 int s3;
 
 void setup()
 {
-    Wire.begin(SLAVE_ADDRESS); 
-    Wire.onReceive(receiveData); 
-    Wire.onRequest(sendData); 
+    Wire.begin(SLAVE_ADDRESS); // Set the pin of I2C
+    Wire.onReceive(readData); // When receive the data, read using I2C
+    Wire.onRequest(sendData); // When send the data, send using I2C
 }
 
 void sendData()
 {
-  int se = 1
-  Wire.write(se);
+  Wire.write(s3); // Send data to EV3
 }
 
-void receiveData()
+void readData()
 {
-    while(Wire.available()>0)
+    while(Wire.available()>0) // Verify if the Arduino is connected to EV3
     {
-      int s2 = Wire.read();
+      int s2 = Wire.read(); // Read data sent by EV3
+      // Discard 7 bytes unused
+      for (int i = 0; i = 7; i++) {
+        Wire.read();
+      }
     }
 }
+

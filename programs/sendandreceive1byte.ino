57d8e222509b8893837982d026bce2eeb362deae
Alisson Silva <alissonsilva.py@gmail.com>
2019-12-19 19:41:32 +0000

    Update sendandreceive1byte.ino

--- programs/sendandreceive1byte.ino
+++ programs/sendandreceive1byte.ino
@@ -14,7 +14,7 @@ int s3; // Variable used to receive data from EV3
 
 void setup()
 {
-    Serial.begin(9600); // Configure Serial with baud 9600;
+    Serial.begin(9600); // Configure Serial with baud 9600
     
     Wire.begin(SLAVE_ADDRESS); // Set the pin of I2C
     Wire.onReceive(readData); // When receive the data, read using I2C
